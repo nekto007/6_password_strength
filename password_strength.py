f88e8f85c5a95f811a782d38aed5fc424400c7e2
Igor <nekto.korobko@gmail.com>
2017-04-19 23:17:17 +0300

    Улучшение руководство пользователя и улучшение скрипта

--- password_strength.py
+++ password_strength.py
@@ -1,31 +1,36 @@
 import re
+import os, sys
+check = 1
 
 
 def get_password_strength(password):
-    check = 1
+    global check
     if re.search('.\d', password):
         check += 1
+        print('1')
     if re.search('[a-z]', password):
         check += 1
     if re.search('[A-Z]', password):
         check += 2
     if re.search('.\W', password):
         check += 2
+        print(2)
     if len(password) >= 8:
         check += 1
-    check += check_password_blacklist(password)
-    print('Сложность вашего пароля: ', check)
+    check = check_password_blacklist(password, check)
 
 
-def check_password_blacklist(password):
-    with open('blacklist.txt', 'r') as blacklist:
-        check = 2
-        for i in blacklist:
-            if password in i:
-                check = -2
-                break
+def check_password_blacklist(password, check):
+    file = open('blacklist.txt', 'r').read().split()
+    if str(password) in file:
+        check = 1
+    else:
+        check += 2
     return check
 
 
 if __name__ == '__main__':
-    get_password_strength(input('Введите пароль: '))
+    password = input('Введите Ваш пароль: ')
+    os.system('clear')
+    get_password_strength(password)
+    print('Сложность Вашего пароля: %s/10' % (check))
